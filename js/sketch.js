d7fd89d587e0e3503531150ad232ab3f49b498c8
Isidro López Pacheco <L24170608@culiacan.tecnm.mx>
2026-02-16 10:17:28 -0700

    Add files via upload

---
+++ js/sketch.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(1300, 5);
+  background("220");
+}
+
+function draw() {
+  //izquierdo
+  stroke(0);
+  line(30, 20, 185, 20);
+  stroker(126);
+  line(185, 20, 185, 175);
+  stroker(255);
+  line(185, 175, 30, 175);
+  line(30, 175, 30, 20);
+  //derecho
+  stroke(0);
+  line(30, 20, 185, 20);
+  stroker(126);
+  line(185, 20, 185, 175);
+  stroker(255);
+  line(185, 175, 30, 175);
+  line(30, 175, 30, 20);
+
+
+
+
+}
